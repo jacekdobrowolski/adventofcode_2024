489f08acbc4cb7cfa762dae611a0aab5af4a3f0c
Jacek Dobrowolski <jacek.ambrozy.dobrowolski@gmail.com>
2024-12-05 23:00:43 +0100

    day 5 part 1

---
+++ 5/README.md
@@ -0,0 +1,3 @@
+day 5 part 1 took 91.34µs
+day 5 part 1 result 6612
+
---
+++ 5/main.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"log"
+	"os"
+	"runtime/pprof"
+	"slices"
+	"sync"
+	"time"
+)
+
+const ITERATIONS = 10_000
+
+//go:embed input
+var input []byte
+
+type Num = [2]byte
+type Manual = []Num
+
+func main() {
+
+	if cpuProfile := os.Getenv("CPU_PROFILE"); cpuProfile != "" {
+		f, err := os.Create(cpuProfile)
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		}
+		defer f.Close()
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
+
+		defer pprof.StopCPUProfile()
+	}
+
+	minTook := time.Hour
+	var result = 0
+
+	for range ITERATIONS {
+		start := time.Now()
+
+		rulebook, manuals := ParseInput(input)
+		result = 0
+		results := make(chan int)
+
+		go func() {
+			for res := range results {
+				result += res
+			}
+		}()
+
+		wg := sync.WaitGroup{}
+
+		for _, manual := range manuals {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				if Valid(manual, rulebook) {
+					results <- ParseNum(manual[len(manual)/2])
+				}
+			}()
+		}
+		wg.Wait()
+		close(results)
+
+		took := time.Since(start)
+		if took < minTook {
+			minTook = took
+		}
+	}
+	fmt.Printf("day 5 part 1 took %v\n", minTook)
+	fmt.Printf("day 5 part 1 result %d\n", result)
+
+}
+func ParseNum(num Num) int {
+	return 10*int(num[0]-'0') + int(num[1]-'0')
+}
+func Valid(m Manual, rulebook map[Num][]Num) bool {
+	forbidenPages := make([]Num, 0, 29*24)
+	for _, page := range m {
+		if slices.Contains(forbidenPages, page) {
+			return false
+		}
+
+		if pages, ok := rulebook[page]; ok {
+			forbidenPages = append(forbidenPages, pages...)
+		}
+	}
+
+	return true
+}
+
+func ParseInput(input []byte) (map[Num][]Num, []Manual) {
+	rulebook := make(map[Num][]Num, 81)
+	manuals := make([]Manual, 0, 210)
+
+	var firstPage, secondPage Num
+	var pipe byte
+	i := 0
+
+	for {
+		firstPage, pipe, secondPage = (Num)(input[i:2+i]), input[2+i], (Num)(input[3+i:5+i])
+
+		if pipe != byte('|') {
+			break
+		}
+
+		i += 6
+		rulebook[secondPage] = append(rulebook[secondPage], firstPage)
+
+	}
+
+	i += 1 // move over the empy line
+	for i < len(input) {
+		if input[i] == '\n' {
+			break
+		}
+
+		manual := make(Manual, 0, 29)
+	MANUAL:
+		for i+2 < len(input) {
+
+			firstPage = (Num)(input[i : i+2])
+
+			manual = append(manual, firstPage)
+
+			if input[i+2] == byte('\n') {
+				i += 3
+				break MANUAL
+			}
+
+			i += 3
+		}
+
+		manuals = append(manuals, manual)
+	}
+	return rulebook, manuals
+}
+
+func PrintRuleBook(r map[Num][]Num) {
+	for key, items := range r {
+		fmt.Printf("%s: {", key)
+		for _, item := range items {
+			fmt.Printf("%s, ", item)
+		}
+		fmt.Printf("}\n")
+	}
+}
+
+func PrintManuals(m []Manual) {
+	for i, manual := range m {
+		fmt.Printf("%d - ", i)
+		for _, item := range manual {
+			fmt.Printf("%s, ", item)
+		}
+		fmt.Printf("\n")
+	}
+}
